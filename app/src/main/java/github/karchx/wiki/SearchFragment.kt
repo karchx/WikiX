a15270c4a1fcdbbc029d087f948e4d4706b1f122
AndrusKarchevskiy <karch.andrus@gmail.com>
2021-04-03 23:10:06 +0300

    Added getting base page of url (async task)

--- app/src/main/java/github/karchx/wiki/SearchFragment.kt
+++ app/src/main/java/github/karchx/wiki/SearchFragment.kt
@@ -1,10 +1,18 @@
+/*
+ * Copyright 2021 Andrey Karchevsky <karch.andrus@gmail.com>
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package github.karchx.wiki
 
+import android.os.AsyncTask
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import github.karchx.wiki.tools.search_engine.SearchEngine
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,6 +34,23 @@ class SearchFragment : Fragment() {
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
+        }
+        val url = "https://ru.wikipedia.org/w/api.php?action=opensearch&search=%EC%E0%F1%F2%E5%F0%20%E8%20%EC%E0%F0%E3%E0%F0%E8%F2%E0&prop=info&format=xml&inprop=url"
+        val contentTask = GetBasePage(url)
+        contentTask.execute()
+    }
+
+    private class GetBasePage(val url: String) : AsyncTask<String, String, String>() {
+
+        override fun doInBackground(vararg params: String?): String {
+            val engine = SearchEngine()
+            val content = engine.getBasePage(url)
+            return content.toString()
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            // Here xml (string) of base page
         }
     }
 
