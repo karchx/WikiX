76048bfaa739bd5173c0e3eb26f0e609f7094fff
AndrusKarchevskiy <karch.andrus@gmail.com>
2021-04-07 22:32:00 +0300

    Added layouts; forming response from user request (text input layout); added stub of app icon

--- app/src/main/java/github/karchx/wiki/SearchFragment.kt
+++ app/src/main/java/github/karchx/wiki/SearchFragment.kt
@@ -11,25 +11,21 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import github.karchx.wiki.tools.search_engine.SearchEngine
 import java.util.*
-import kotlin.collections.ArrayList
+
 
 class SearchFragment : Fragment() {
 
     private val engine = SearchEngine()
+    private var mSearchBtn: Button? = null
+    private var mUserRequest: EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val userLang = Locale.getDefault().language
-
-        // Param `request` -- user's request (in search textInput field)
-        val url = engine.formUrlPages(userLang, request = "Kotlin")
-
-        val contentTask = GetListOfPages(url)
-        // Start process with getting List of Pages
-        contentTask.execute()
     }
 
     private class GetListOfPages(val url: String) :
@@ -41,17 +37,33 @@ class SearchFragment : Fragment() {
             return engine.getPagesInfo(content!!)
         }
 
-        override fun onPostExecute(result: ArrayList<ArrayList<String>>?) {
-            super.onPostExecute(result)
-            Log.d("ResultOfGetting", result.toString())
+        override fun onPostExecute(response: ArrayList<ArrayList<String>>?) {
+            super.onPostExecute(response)
+            Log.d("UserRequest", response.toString())
         }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.search_fragment, container, false)
+    ): View {
+        val view: View = inflater.inflate(R.layout.search_fragment, container, false)
+
+        mSearchBtn = view.findViewById(R.id.searchButton) as Button
+        mSearchBtn!!.setOnClickListener {
+            mUserRequest = view.findViewById(R.id.editTextUserRequest)
+            val userRequest = mUserRequest!!.text.toString()
+            val userLang = Locale.getDefault().language
+
+            // Param `request` -- user's request (in search textInput field)
+            val url = engine.formUrlPages(userLang, request = userRequest)
+
+            val contentTask = GetListOfPages(url)
+            // Start process with getting List of Pages
+            contentTask.execute()
+        }
+
+        // Return the fragment view/layout
+        return view
     }
 }
