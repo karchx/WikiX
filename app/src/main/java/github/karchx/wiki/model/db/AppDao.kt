144e91fffa01b4f03808ac909dcc0ca64eccd0c4
AndrusKarchevskiy <karch.andrus@gmail.com>
2021-04-11 21:49:12 +0300

    Added displaying articles (recyclerView); added demo db settings

--- app/src/main/java/github/karchx/wiki/model/db/AppDao.kt
+++ app/src/main/java/github/karchx/wiki/model/db/AppDao.kt
@@ -13,21 +13,15 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface AppDao {
-    @Query("SELECT * FROM user")
-    fun getAll(): List<User>
+    @Query("SELECT * FROM articles")
+    fun getAll(): List<Articles>
 
-//    @Query("SELECT * FROM user WHERE uid IN (:userIds)")
-//    fun loadAllByIds(userIds: IntArray): Flow<User>
-    @Query("SELECT * FROM user WHERE uid = :userId")
-    fun load(userId: String): Flow<User>
-
-    @Query("SELECT * FROM user WHERE first_name LIKE :first AND " +
-            "last_name LIKE :last LIMIT 1")
-    fun findByName(first: String, last: String): User
+    @Query("SELECT * FROM articles WHERE aid = :articleId")
+    fun load(articleId: String): Flow<Articles>
 
     @Insert
-    fun insertAll(vararg users: User)
+    fun insertAll(vararg articles: Articles)
 
     @Delete
-    fun delete(user: User)
+    fun delete(article: Articles)
 }
