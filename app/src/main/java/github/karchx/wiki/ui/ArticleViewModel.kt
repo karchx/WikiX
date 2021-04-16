588dbc2609089230261e49eb24c635d602f21021
AndrusKarchevskiy <karch.andrus@gmail.com>
2021-04-16 17:26:26 +0300

    Improved some colors, search Fragment design features added

--- app/src/main/java/github/karchx/wiki/ui/ArticleViewModel.kt
+++ app/src/main/java/github/karchx/wiki/ui/ArticleViewModel.kt
@@ -25,8 +25,7 @@ class ArticleViewModel @Inject constructor(
         viewModelScope.launch {
             val articlePage = repository.fetchArticlePage( pageUrl )
             if( articlePage != null ) {
-                val pageStyle =
-                        "<link rel=\"stylesheet\" href=\"/w/load.php?lang=en&amp;modules=ext.cite.styles%7Cext.wikimediaBadges%7Cmediawiki.hlist%7Cmediawiki.ui.button%2Cicon%7Cmobile.init.styles%7Cskins.minerva.base.styles%7Cskins.minerva.content.styles%7Cskins.minerva.content.styles.images%7Cskins.minerva.icons.wikimedia%7Cskins.minerva.mainMenu.icons%2Cstyles&amp;only=styles&amp;skin=minerva\"/>\n"
+                val pageStyle = "<link rel=\"stylesheet\" href=\"/w/load.php?lang=en&amp;modules=ext.cite.styles%7Cext.wikimediaBadges%7Cmediawiki.hlist%7Cmediawiki.ui.button%2Cicon%7Cmobile.init.styles%7Cskins.minerva.base.styles%7Cskins.minerva.content.styles%7Cskins.minerva.content.styles.images%7Cskins.minerva.icons.wikimedia%7Cskins.minerva.mainMenu.icons%2Cstyles&amp;only=styles&amp;skin=minerva\"/>\n"
                 articlePage.text = pageStyle + articlePage.text
                 _articlePage.value = articlePage!!
             }
