d40bb65c52f69a2987f66e4fdce2713ee53817dc
AndrusKarchevskiy <karch.andrus@gmail.com>
2021-04-21 23:24:35 +0300

    Renaming

--- app/src/main/java/github/karchx/wiki/tools/news_engine/NewsArticle.kt
+++
@@ -1,7 +0,0 @@
-package github.karchx.wiki.tools.news_engine
-
-data class NewsArticle(val title: String,
-                       val description: String,
-                       val datePublished: String,
-                       val urlToImage: String,
-                       val url: String)
