7785c5649f838a25dd4f46e824ee430ad4fbba1f
AndrusKarchevskiy <karch.andrus@gmail.com>
2021-04-23 00:43:10 +0300

    Added recycler with News articles

--- app/src/main/java/github/karchx/wiki/tools/news_engine/NewsArticleItem.kt
+++ app/src/main/java/github/karchx/wiki/tools/news_engine/NewsArticleItem.kt
@@ -1,7 +1,11 @@
 package github.karchx.wiki.tools.news_engine
 
-data class NewsArticleItem(val title: String,
-                           val description: String,
-                           val datePublished: String,
-                           val urlToImage: String,
-                           val url: String)
+import android.graphics.Bitmap
+
+data class NewsArticleItem(
+    val title: String,
+    val description: String,
+    val datePublished: String,
+    val urlToImage: String,
+    val url: String
+)
