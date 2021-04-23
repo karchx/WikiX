728ad9ce9d385086a7588583c6c49a089e441ec7
AndrusKarchevskiy <karch.andrus@gmail.com>
2021-04-23 16:51:07 +0300

    Updated recycler design, optimization: only first 10 news articles of response adds to recycler

--- app/src/main/java/github/karchx/wiki/tools/DrawableManager.kt
+++ app/src/main/java/github/karchx/wiki/tools/DrawableManager.kt
@@ -1,7 +1,9 @@
 package github.karchx.wiki.tools
 
+import android.app.Activity
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.util.DisplayMetrics
 import java.io.InputStream
 import java.net.HttpURLConnection
 import java.net.URL
@@ -10,18 +12,34 @@ import java.net.URL
 class DrawableManager {
 
     companion object {
-        fun getDrawable(url: String): Bitmap {
+        fun getDrawable(activity: Activity, url: String): Bitmap {
+            val displayMetrics = DisplayMetrics()
+            activity.windowManager.defaultDisplay.getMetrics(displayMetrics)
+            val height = displayMetrics.heightPixels / 5
+            val width = displayMetrics.widthPixels / 2
+
             try {
                 val connection: HttpURLConnection = URL(url).openConnection() as HttpURLConnection
                 connection.connect()
                 val input: InputStream = connection.inputStream
-                return Bitmap.createScaledBitmap(BitmapFactory.decodeStream(input), 600, 400, true)
+                return Bitmap.createScaledBitmap(
+                    BitmapFactory.decodeStream(input),
+                    width,
+                    height,
+                    true
+                )
             }
             catch (ex: Exception) {
-                val connection: HttpURLConnection = URL("https://angliyskiyazik.ru/wp-content/uploads/2018/05/news.jpg").openConnection() as HttpURLConnection
+                val connection: HttpURLConnection =
+                    URL("https://angliyskiyazik.ru/wp-content/uploads/2018/05/news.jpg").openConnection() as HttpURLConnection
                 connection.connect()
                 val input: InputStream = connection.inputStream
-                return Bitmap.createScaledBitmap(BitmapFactory.decodeStream(input), 600, 400, true)
+                return Bitmap.createScaledBitmap(
+                    BitmapFactory.decodeStream(input),
+                    width,
+                    height,
+                    true
+                )
             }
         }
     }
